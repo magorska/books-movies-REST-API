824be80394b63343b2d3d28dccb4006e44f042b9
magorska <magorska.95@gmail.com>
2021-08-21 11:57:15 +0200

    First Commit

---
+++ src/main/java/com/booksmoviesapp/BooksMoviesAppApplication.java
@@ -0,0 +1,13 @@
+package com.booksmoviesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksMoviesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksMoviesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booksmoviesapp/BooksMoviesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booksmoviesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksMoviesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
